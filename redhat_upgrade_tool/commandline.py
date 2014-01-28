rhinstaller/fedup

fix UnboundLocalError with fedup --device
The Great Denaming accidentally made 'fedup.media' and a local variable
named 'media' collide. Rename the local variable to fix that.

fedup/commandline.py
@@ -149,19 +149,19 @@ class RepoAction(argparse.Action):
 # check the argument to '--device' to see if it refers to install media
 def device_or_mnt(arg):
     if arg == 'auto':
-        media = media.find()
+        localmedia = media.find()
     else:
-        media = [m for m in media.find() if arg in (m.dev, m.mnt)]
+        localmedia = [m for m in media.find() if arg in (m.dev, m.mnt)]
 
-    if len(media) == 1:
-        return media.pop()
+    if len(localmedia) == 1:
+        return localmedia.pop()
 
-    if not media:
+    if not localmedia:
         msg = _("no install media found - please mount install media first")
         if arg != 'auto':
             msg = "%s: %s" % (arg, msg)
     else:
-        devs = ", ".join(m.dev for m in media)
+        devs = ", ".join(m.dev for m in localmedia)
         msg = _("multiple devices found. please choose one of (%s)") % devs
     raise argparse.ArgumentTypeError(msg)
 

stormspirit/redhat-upgrade-tool

(cherry picked from commit 2c723f7aab47b6b6cf21d12a7962f15f50174f07)

redhat_upgrade_tool/commandline.py
@@ -147,19 +147,19 @@ def device_or_mnt(option, opt, value):
         value = 'auto'
 
     if value == 'auto':
-        media = media.find()
+        localmedia = media.find()
     else:
-        media = [m for m in media.find() if value in (m.dev, m.mnt)]
+        localmedia = [m for m in media.find() if value in (m.dev, m.mnt)]
 
-    if len(media) == 1:
-        return media.pop()
+    if len(localmedia) == 1:
+        return localmedia.pop()
 
-    if not media:
+    if not localmedia:
         msg = _("no install media found - please mount install media first")
         if value != 'auto':
             msg = "%s: %s" % (value, msg)
     else:
-        devs = ", ".join(m.dev for m in media)
+        devs = ", ".join(m.dev for m in localmedia)
         msg = _("multiple devices found. please choose one of (%s)") % devs
     raise optparse.OptionValueError(msg)
 